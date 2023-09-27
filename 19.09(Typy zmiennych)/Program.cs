5c184e78a03bb6642e90c6321159ff0652f819cd
Miłosz Michalak <114921866+MilMichalak@users.noreply.github.com>
2023-09-27 21:21:02 +0200

    Dodanie plikow

---
+++ 19.09(Typy zmiennych)/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+namespace PierwszyProgram
+{
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Pierwszy program");
+            Console.WriteLine("W jezyku C#");
+            Console.WriteLine("Test");
+            Console.WriteLine("C#");
+
+            /*
+            * Typy danych - ile bitow
+            * int 16, 32, 64
+            * byte 0-255 - (8 bitow => 1 bajt)
+            * sbyte -128 do 127 (8 bitow => 1 bajt)
+            * short -32768 do 32767 (16 bitow => 2 bajty)
+            * ushort 0 do 65535 (16 bitow => 2 bajty)
+            * int -2 147 483 648 do 2 147 483 647 (32 bity => 4 bajty)
+            * uint 0 - 4 294 967 295 (32 bity => 4 bajty)
+
+            * Na kartowke long jest niepotrzebny
+            * long -9223372036854775808 do 9223372036854775807 (64 bity => 8 bajtow)
+            * ulong 0 do 18 446 744 073 709 551 615(64 bity => 8 bajtów, 18 trylionów)
+            *
+            * bool (true or false) - ile bitów/bajtów
+            * char U+0000 do U+FFFF (16 bitowy znak z tablicy unicode)
+            * string ?
+            *
+            * float -3,4 * 10^38 do 3,4 * 10^38 (32 bajty => 4 bity)
+            * double (64 bity => 8 bajtow)
+            * decimal (128 bitów => 16 bajtów)
+            */
+
+            int i; //deklaracja zmiennej
+            int i1 = 10; // deklaracje z jednoczesna inicjalizacja zmiennej i
+
+            Console.WriteLine("Zmienna i wynosi: " + i1);
+
+            System.Int32 i2 = 100;
+
+            Console.WriteLine("Zmienna i1 wynosi:  {0}, zmienna i2 wynosi:  {1}", i1, i2);
+            Console.WriteLine($"Zmienna i1 wynosi: {i1}, zmienna i2 wynosi: {i2}");
+
+            byte b = 10;
+            sbyte sb = -10;
+            short s = -20000;
+
+            // Wszystkie zapisy oznaczaja to samo
+            int incior = 2200000;
+            Int32 takiInt = 10;
+            System.Int32 jeszczeInny = 299;
+
+            long l = -10L; //suffix L
+            ulong ul = 10UL; // suffix UL
+            
+            Console.WriteLine("Zmienna l: {0}, zmienna ul: {1}", l, ul);
+
+            float f = 10.5F; // suffix F
+            
+            
+
+            Console.ReadKey();
+
+        }
+    }
+}
+
---
+++ 26.09 systemy liczbowe i unicode/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+// zapis systemow liczbowych
+// binarny
+System.Console.WriteLine("Zamiana systemow liczbowych");
+Console.WriteLine(0b1011); //11
+
+// oktalny
+Console.WriteLine(011); // 11
+int io = Convert.ToInt32("11", 8);
+Console.WriteLine(io); // 10 => 2x8^0 + 1x8^1 = 2 + 8 = 10(10)
+
+// Heksadecymalny
+Console.WriteLine(0xA1); // 161(10) => Ax16^1 + Ax16^0 = 10x16 + 1x1 = 160 + 1 = 161(10)
+
+System.Console.WriteLine();
+
+// Zakres typów danych
+System.Console.WriteLine("Wypisywanie zakresow typow zmiennych");
+Console.WriteLine(byte.MinValue); // 0
+Console.WriteLine(byte.MaxValue); // 255
+
+Console.WriteLine(sbyte.MinValue); // -128
+Console.WriteLine(sbyte.MaxValue); // 127
+
+// Pokazane ze te 3 rodzaje to jest to samo 
+Console.WriteLine(int.MaxValue);
+Console.WriteLine(Int32.MaxValue);
+Console.WriteLine(System.Int32.MaxValue);
+
+System.Console.WriteLine(long.MinValue);
+System.Console.WriteLine(long.MaxValue);
+
+System.Console.WriteLine(decimal.MinValue);
+System.Console.WriteLine(decimal.MaxValue);
+
+// w dotnet 8 Int128 nie istnieje
+System.Console.WriteLine(Int128.MaxValue);
+
+System.Console.WriteLine();
+
+// UNICODE (strona do ascii - asciitable.com)
+System.Console.WriteLine("Unicode");
+// Kodowanie powinno dzialac - windows, na linuxie ma lekki problem
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+System.Console.WriteLine("♥");
+Console.OutputEncoding = System.Text.Encoding.Default;
+
+// Wyswietla serce poprzez kod unicode
+System.Console.WriteLine("\u2665");
+
+// zapisywanie litery jako znak (char) ascii
+char letter = 'a';
+System.Console.WriteLine(letter);
+
+// zeby jako char przerobic cyfre na litere trzeba rzutowac cyfre na chara
+char letter2 = (char)97;
+System.Console.WriteLine(letter2);
+
