98e3c3113d031adc22606fc62f9666223e14ecba
MilMichalak <miloszmichalak150@gmail.com>
2023-09-27 22:27:16 +0200

    Uzupełnienie lekcji

--- Instrukcje warunkowe/script.js
+++ Instrukcje warunkowe/script.js
@@ -16,8 +16,20 @@ let informacja = document.getElementById("informacja");
 //     alert("Podałeś błędny login lub hasło");
 // }
 
-// * Funkcja ktora zmienia znak na cyfre w kodzie ascii, index 0 oznacza ze zamienia tylko 1 znak na ascii
+// * Funkcja ktora sprawdza haslo nie podaje wprost ze wynosi ono "8" tylko sprawdza czy jest to 8 za pomoca kodu ASCII
 function szyfrowanie(){
+    let login = prompt("Podaj swoj login", "login");
+    let password = prompt("Podaj swoje hasło", "hasło");
+    let haslo = password.charCodeAt(0);
+    if (login == "admin" && haslo == 56){
+        informacja.innerText = "Udalo ci sie zalogowac";
+    } else {
+        alert("Podałeś błędny login lub hasło");
+    }
+}
+
+// * Funkcja ktora zmienia znak na cyfre w kodzie ascii, index 0 oznacza ze zamienia tylko 1 znak na ascii
+function szyfrowanieZnaku(){
     let litera = prompt("Podaj znak do zaszyfrowania", "");
     alert("Zaszyfrowany znak: (cyfra w kodzie ascii) - " + litera.charCodeAt(0));
 }
