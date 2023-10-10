ebbd2606cc5acc003b8e64eec480518cf15e8c19
MilMichalak <miloszmichalak150@gmail.com>
2023-10-10 15:48:01 +0200

    Dodanie kartkowki

---
+++ 10.10 kartkowka/Program.cs
@@ -0,0 +1,31 @@
+﻿// Numer 3 - Grupa 1
+// Zadanie 1 - Buzka z usmieszkiem, serduszko do wypisania
+System.Console.WriteLine("Zadanie 1");
+
+System.Console.WriteLine("\u263A");
+System.Console.WriteLine("\u2665");
+
+System.Console.WriteLine();
+
+// Zadanie 2 - opisac liczby calkowite
+System.Console.WriteLine("Zadanie 2");
+// byte od -127 do 128 | 8bitow -> 1 bajt
+// sbyte od 0 do 255 | 8 bitow -> 1 bajt
+// short od -32768 do 32767 | 16 bitow -> 2 bajty
+// ushort od 0 do 65535 | 16 bitow -> 2 bajty
+// int od -2147483648 do 2147483647 | 32bity -> 4 bajty
+// uint od 0 do 4294967295 | 32 bity -> 4 bajty
+// long | 64 bity -> 8 bajtow
+// ulong od 0 do | 64 bity -> 8 bajtow
+
+System.Console.WriteLine();
+
+// Zadanie 3 - obwod koła 2pr
+System.Console.WriteLine("Zadanie 3");
+double wynik = 0;
+double r = Convert.ToDouble(System.Console.ReadLine());
+
+
+wynik = 2 * Math.PI * r;
+System.Console.WriteLine(wynik);
+
