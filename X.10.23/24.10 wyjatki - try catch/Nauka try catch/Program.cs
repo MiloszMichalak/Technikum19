3b46604b435d8be2830ed9cddec5f92e4c7e377b
Miłosz Michalak <miloszmichalak150@gmail.com>
2023-10-24 19:10:54 +0200

    Posortowanie plikow i dodanie lekcji z 24 10

---
+++ X.10.23/24.10 wyjatki - try catch/Nauka try catch/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace wyjatki
+{
+    class Program{
+
+        static void ErrorColorChange(string comment)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Błąd: {comment}");
+            Console.ResetColor();
+        }
+        
+        static void Main(string[] args)
+        {
+            bool flagunia = true;
+            while (flagunia)
+            {
+                try
+                {
+                    System.Console.WriteLine("Podaj 2 liczby\n");
+                    System.Console.Write("Podaj a: ");
+                    int a = Convert.ToInt32(Console.ReadLine());
+                    System.Console.Write("Podaj b:  ");
+                    int b = Convert.ToInt32(Console.ReadLine());
+                    // if (b == 0){
+                    //     throw new Exception("Nie mozna dzielić przez 0");
+                    // }
+                    int result = a / b;
+                    // Tekst ponizej zostanie wypisany na zielono
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    System.Console.WriteLine("Wynik dzielenia: " + result);
+                    flagunia = false;
+                    Console.ResetColor();
+                }
+                catch (DivideByZeroException)
+                {
+                    ErrorColorChange("Nie mozna dzielic przez 0");
+                }
+                catch (FormatException)
+                {
+                    // wyrzuci wtedy kiedy uzytkownik nie poda inta
+                    ErrorColorChange("Blad formatu danych, podaj liczbe zmiennoprzecinkowa lub calkowita");
+                }
+                catch (OverflowException)
+                {
+                    ErrorColorChange(
+                        $"Bledny zakres liczby, podaj liczbe z zakresu: <{double.MinValue} ; {double.MaxValue}>");
+                }
+                catch (Exception ex)
+                {
+                    ErrorColorChange(ex.Message);
+                }
+            }
+        }
+    }
+}
---
+++ X.10.23/24.10 wyjatki - try catch/Praca na lekcji - wyjatki/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Praca_na_lekcji___wyjatki
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            // Grupa 1
+            // Napisz program ktory oblicza pole prostokata
+            bool flaga = true;
+            while (flaga)
+            {
+                try
+                {
+                    Console.WriteLine("Podaj 2 boki:\n");
+                    Console.Write("Podaj bok 1: ");
+                    double bok1 = double.Parse(Console.ReadLine()!);
+                    Console.Write("Podaj bok 2: ");
+                    double bok2 = double.Parse(Console.ReadLine()!);
+                    if (!(bok1 > 0) || !(bok2 > 0))
+                    {
+                        throw new Exception("Podane boki powinny byc dodatnie");
+                    }
+                    Console.WriteLine();
+                    flaga = false;
+                    double wynik = bok1 * bok2;
+                    Console.WriteLine("Pole prostokata wynosi: {0}", wynik);
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Podaj liczbe zmiennoprzecinkowa lub calkowita");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine($"Podana liczba jest za duza dla danego typu, podaj liczbe z zakresu <{double.MinValue} ; {double.MaxValue}>");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Blad: {ex.Message}");
+                }
+            }
+        }
+    }
+}
