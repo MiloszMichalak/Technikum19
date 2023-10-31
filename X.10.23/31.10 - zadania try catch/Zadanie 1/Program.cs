9e82c553c6949a0e802c2669a57810d508e6b43b
Miloszmichalak <miloszmichalak150@gmail.com>
2023-10-31 12:16:29 +0100

    Dodano lekcje z 31.10

---
+++ X.10.23/31.10 - zadania try catch/Zadanie 1/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Zadania;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string? liczba;
+        do
+        {
+            try
+            {
+                Console.WriteLine("Podaj liczbe: ");
+                liczba = Console.ReadLine();
+                if (liczba.Equals("q"))
+                {
+                    Console.WriteLine("Dziekujemy za skorzystaniew programu");
+                    return;
+                }
+                if (Convert.ToInt32(liczba) < 1)
+                {
+                    throw new ArgumentOutOfRangeException("Podaj liczbe naturalna");
+                }
+
+                Console.WriteLine("Pierwiastek kwadratowy z wpisanej liczby wynosi {0}",
+                    Math.Sqrt(Convert.ToInt32(liczba)));
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                // Dzieki temu ze dokladnie wypisujemy nazwa bledu nie wypisuje calej dlugosci bledu
+                Console.WriteLine("Blad: " + ex.Message);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Wprowadz liczbe");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine($"Przekoczono zakres. Zakres liczby: <{int.MinValue} ; {int.MaxValue}>");
+            }
+        } while (true);
+    }
+}
---
+++ X.10.23/31.10 - zadania try catch/Zadanie 2/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace Zadanie2;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string? liczba = "0";
+        int ile = 0;
+        int[] array = new int[5];
+        while (ile < 5)
+        {
+            try
+            {
+                Console.Write("Podaj liczbe: ");
+                liczba = Console.ReadLine();
+                array[ile] = Convert.ToInt32(liczba);
+                ile++;
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Powinienes podac liczbe");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine(
+                    $"Podales za duza liczbe, powinienes poddac liczbe z zakresu <{int.MinValue} ; {int.MaxValue}>");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Blad {ex.Message}");
+            }
+        }
+
+        Console.WriteLine();
+
+        while (true)
+        {
+            try
+            {
+                Console.Write("Podaj jaki chcesz wyswietlic index z tablicy: ");
+                string miejsce = Console.ReadLine();
+                if (miejsce.Equals("q"))
+                {
+                    return;
+                }
+                Console.WriteLine($"array[{miejsce}] = {array[Convert.ToInt32(miejsce)]}");
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine("Blad: " + ex.Message);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Powinienes podac liczbe");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine(
+                    $"Podales za duza liczbe, powinienes poddac liczbe z zakresu <{int.MinValue} ; {int.MaxValue}>");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Blad {ex.Message}");
+            }
+        }
+    }
+}
